196899aa2639d2752bb665a1587ab622bd0b85c3
vinibrenobr11 <vinibrenobr11@hotmail.com>
2017-10-06 23:38:57 -0300

    Added License view

--- app/src/main/java/br/vinibrenobr11/mathtrab/MainActivity.java
+++ app/src/main/java/br/vinibrenobr11/mathtrab/MainActivity.java
@@ -1,10 +1,13 @@
 package br.vinibrenobr11.mathtrab;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 /**
@@ -13,6 +16,28 @@ import android.view.View;
  */
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.license) {
+
+            AlertDialog.Builder license_dlg = new AlertDialog.Builder(this);
+            license_dlg.setMessage(getString(R.string.MITlicense));
+            license_dlg.setNeutralButton("OK", null);
+            license_dlg.setCancelable(false);
+
+            license_dlg.create().show();
+        }
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
